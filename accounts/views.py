1b907bb61d0e73207a63d62e7682e735983748fc
Yulia51188 <sun51188@gmail.com>
2021-12-15 17:05:05 +0300

    Add games details to profile page

--- accounts/views.py
+++ accounts/views.py
@@ -2,21 +2,33 @@ from django.shortcuts import get_object_or_404, render
 from accounts.models import User
 
 
+def serialize_users(users):
+    user_names = [f'{user.first_name} {user.last_name}' for user in users]
+    return ', '.join(user_names)
+
+
 def serialize_game(game, user):
-    administrators = [f'{administrator.first_name} {administrator.last_name}'
-                      for administrator in game.administrators.all()]
     return {
         'name': game.name,
         'is_admin': user in game.administrators.all(),
-        'admins': ', '.join(administrators),
+        'admins': serialize_users(game.administrators.all()),
         'participants_count': game.participants.count(),
         'id': game.id,
+        'created_at': game.created_at,
+        'gift_cost_limit': game.get_gift_cost_limit_display(),
+        'registration_deadline': game.registration_deadline,
+        'gift_sending_deadline': game.gift_sending_deadline,
+        'participants': serialize_users(game.participants.all()),
     }
-
+         
 
 def profile(request, profile_id):
     user = get_object_or_404(
-        User.objects.prefetch_related('games__administrators'),
+        (
+            User.objects
+            .prefetch_related('games__administrators')
+            .prefetch_related('games__participants')
+        ),
         id=profile_id
     )
     
