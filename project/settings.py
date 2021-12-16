f6b79df387609b5cd846605891e1b5fdaee9746d
Oleg Tarasov <tolegu@ya.ru>
2021-12-16 12:52:15 +0300

    настройки почтового сервера

--- project/settings.py
+++ project/settings.py
@@ -121,3 +121,11 @@ AUTH_USER_MODEL = 'accounts.User'
 
 if DEBUG:
     INTERNAL_IPS = ALLOWED_HOSTS
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = env.str('EMAIL_HOST')
+EMAIL_PORT = env('EMAIL_PORT')
+EMAIL_USE_SSL = env.bool('EMAIL_USE_SSL')
+EMAIL_HOST_PASSWORD = env.str('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = env.str('EMAIL_HOST_USER')
+DEFAULT_FROM_EMAIL = env.str('DEFAULT_FROM_EMAIL')
