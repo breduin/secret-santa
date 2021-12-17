dc2ffa5bb870d084c962981359a42d0681fe614b
Oleg Tarasov <tolegu@ya.ru>
2021-12-17 16:14:29 +0300

    инфо страница после создания игры

--- games/urls.py
+++ games/urls.py
@@ -4,6 +4,7 @@ from .views import MainPageView
 from .views import CreateGameView
 from .views import UpdateGameView
 from .views import get_error_page
+from .views import AfterGameCreationView
 
 
 urlpatterns = [
@@ -11,4 +12,5 @@ urlpatterns = [
     path('create-game/', CreateGameView.as_view(), name='create_game'),
     path('update-game/<int:pk>', UpdateGameView.as_view(), name='update_game'),
     path('error/<int:error_code>', get_error_page, name='error'),
+    path('after-game-creation/', AfterGameCreationView.as_view(), name='after_game_creation'),
 ]
