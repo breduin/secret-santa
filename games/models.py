e4a5cfd69b03ec8a044ae8cd13de216957e99aca
Oleg Tarasov <tolegu@ya.ru>
2021-12-18 10:21:49 +0300

    миграция в Game, поля is_online и your_gift_cost_limit

--- games/models.py
+++ games/models.py
@@ -1,14 +1,16 @@
 from django.conf import settings
+from django.core.validators import MinValueValidator
 from django.db import models
+
 
 
 class Game(models.Model):
     """Игра."""
-    COST_LIMIT_CHOICES = (
-    ('None', 'без ограничений'),
+    COST_LIMIT_CHOICES = (    
     ('UPTO500', 'до 500 рублей'),
     ('500_1000', '500-1000 рублей'),
     ('1000_2000', '1000-2000 рублей'),
+    ('YOUR', 'задать свой лимит'),
     )
 
     name = models.CharField(
@@ -24,7 +26,6 @@ class Game(models.Model):
     place = models.CharField(
         verbose_name='Место встречи', 
         max_length=256,
-        help_text='Оставьте поле пустым, если игра проводится через email-рассылку',
         blank=True,      
         )        
     created_at = models.DateTimeField(
@@ -42,13 +43,18 @@ class Game(models.Model):
         related_name='created_games',
         editable=False,
         )
-
     gift_cost_limit = models.CharField(
         verbose_name='Стоимость подарка', 
         max_length=50,
         choices=COST_LIMIT_CHOICES,
-        default='None',
+        default='UPTO500',
         )    
+    your_gift_cost_limit = models.PositiveIntegerField(
+        verbose_name='Ваш лимит стоимости подарка', 
+        validators=[MinValueValidator(1)],
+        blank=True,
+        null=True,
+        )      
     registration_deadline = models.DateField(
         verbose_name='Дата окончания регистрации',
         )
@@ -72,6 +78,10 @@ class Game(models.Model):
         default=True,
         verbose_name='Буду участником?',
         )
+    is_online = models.BooleanField(
+        default=True,
+        verbose_name='Игра проводится онлайн?',
+        )    
 
     def __str__(self):
         return self.name
